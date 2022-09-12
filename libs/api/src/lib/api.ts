5ac43340589569c4d7f29f290bfff641af9be1b3
chavda-bhavik <bhavikvchavda@gmail.com>
2022-09-12 11:32:51 +0530

    feat: Updated Methods calling

--- libs/api/src/lib/api.ts
+++ libs/api/src/lib/api.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-async-promise-executor */
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosRequestHeaders, AxiosResponse } from 'axios';
 import {
   iConfigType,
   iFetchUrl,
@@ -7,13 +7,14 @@ import {
   iFetchConfig,
   ACTION_TYPES,
   ACTION,
-  iSetHeaders,
 } from './interfaces';
 
 let CONFIG: iConfigType = {
   baseUrl: '',
   handleCache: true,
 };
+
+axios.defaults.headers.common['Authorization'] = 'Bearer test';
 
 export const setAPIConfig = (conf: iConfigType) => {
   CONFIG = {
@@ -25,19 +26,59 @@ export const setAPIConfig = (conf: iConfigType) => {
 let cache: Array<string> = [];
 const cancel: Array<any> = [];
 
-const getEndPoint = (config: iConfigType) => {
-  const finalUrl = CONFIG.baseUrl.endsWith('/')
+const getUrl = (config: iConfigType, resourceUrl: string, query?: string) => {
+  // Sanitizing BaseUrl to always ends with /
+  let baseUrl = CONFIG.baseUrl.endsWith('/')
     ? CONFIG.baseUrl
     : `${config.baseUrl}/`;
+
+  // Adding Prefix if specified
   if (CONFIG.prefix) {
-    const prefix =
+    let prefix =
       typeof CONFIG.prefix === 'function'
         ? CONFIG.prefix(config)
         : CONFIG.prefix;
-    return finalUrl + prefix;
-  } else {
-    return finalUrl;
+    if (prefix) {
+      if (prefix.startsWith('/')) prefix = prefix.substring(1);
+      if (!prefix.endsWith('/')) prefix = prefix + '/';
+      baseUrl = baseUrl + prefix;
+    }
   }
+
+  // Sanitizing resourceUrl and removing / in front of it
+  let appendUrl = streamlineUrl(resourceUrl);
+  if (appendUrl.startsWith('/')) appendUrl = appendUrl.substring(1);
+
+  // Building finalUrl
+  let finalUrl = `${baseUrl}${appendUrl || ''}`;
+  if (query) {
+    finalUrl = finalUrl + `?${query}`;
+  }
+  return finalUrl;
+};
+
+const getHeaders = async (config: iConfigType, fetchConfig?: iFetchConfig) => {
+  let headers: AxiosRequestHeaders = {};
+  const token =
+    typeof config.getToken === 'function'
+      ? await config.getToken()
+      : config.getToken;
+
+  // if token exits then set it to Authorization
+  if (token) {
+    // pass false to avoid any prefix
+    const { tokenPrefix = 'Bearer' } = config;
+    headers['Authorization'] = `${
+      tokenPrefix ? `${tokenPrefix} ` : ''
+    }${token}`;
+  }
+  if (fetchConfig?.headers) {
+    headers = {
+      ...headers,
+      ...fetchConfig.headers,
+    };
+  }
+  return headers;
 };
 
 export function serialize(obj: ObjectType) {
@@ -59,43 +100,26 @@ const streamlineUrl = (url: string): string => {
 
 const ACTION_HANDLERS: { [key in ACTION_TYPES]: ACTION } = {
   GET: (url: string, data: ObjectType, fetchConfig?: iFetchConfig) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let queryUrl = url || '';
-        if (queryUrl.startsWith('/')) queryUrl = queryUrl.substring(1);
-
-        if (data !== undefined) {
-          const query = serialize(data);
-          if (query) queryUrl = `${queryUrl}?${query}`;
-        }
-        const finalUrl = `${getEndPoint(CONFIG)}${url ? queryUrl : ''}`;
-        const response = await axios.get(finalUrl, {
-          // credentials: 'include',
-          // withCredentials: false,
-          cancelToken: new axios.CancelToken((cToken: any) => {
-            cancel.push({ url, cToken });
-          }),
-          headers: fetchConfig?.headers || {},
-        });
-        resolve(response);
-      } catch (error) {
-        reject(error);
-      }
+    const query = data ? serialize(data) : '';
+    const finalUrl = getUrl(CONFIG, url, query);
+    return axios.get(finalUrl, {
+      // credentials: 'include',
+      // withCredentials: false,
+      // cancelToken: new axios.CancelToken((cToken: any) => {
+      //   cancel.push({ url, cToken });
+      // }),
+      headers: fetchConfig?.headers || {},
     });
   },
   DELETE: (url: string, data: ObjectType, fetchConfig: iFetchConfig) => {
-    const finalUrl = streamlineUrl(
-      `${getEndPoint(CONFIG)}${url ? `/${url}` : ''}`
-    );
+    const finalUrl = getUrl(CONFIG, url);
     return axios.delete(finalUrl, {
       data,
       headers: fetchConfig.headers,
     });
   },
   POST: (url: string, data: ObjectType, fetchConfig: iFetchConfig) => {
-    const finalUrl = streamlineUrl(
-      `${getEndPoint(CONFIG)}${url ? `/${url}` : ''}`
-    );
+    const finalUrl = getUrl(CONFIG, url);
     return axios.post(finalUrl, data, {
       // credentials: 'include',
       // withCredentials: true,
@@ -103,9 +127,7 @@ const ACTION_HANDLERS: { [key in ACTION_TYPES]: ACTION } = {
     });
   },
   PATCH: (url: string, data: ObjectType, fetchConfig: iFetchConfig) => {
-    const finalUrl = streamlineUrl(
-      `${getEndPoint(CONFIG)}${url ? `/${url}` : ''}`
-    );
+    const finalUrl = getUrl(CONFIG, url);
     return axios.patch(finalUrl, data, {
       // credentials: 'include',
       // withCredentials: true,
@@ -113,9 +135,7 @@ const ACTION_HANDLERS: { [key in ACTION_TYPES]: ACTION } = {
     });
   },
   PUT: (url: string, data: ObjectType, fetchConfig: iFetchConfig) => {
-    const finalUrl = streamlineUrl(
-      `${getEndPoint(CONFIG)}${url ? `/${url}` : ''}`
-    );
+    const finalUrl = getUrl(CONFIG, url);
     return axios.put(finalUrl, data, {
       // credentials: 'include',
       // withCredentials: true,
@@ -123,36 +143,6 @@ const ACTION_HANDLERS: { [key in ACTION_TYPES]: ACTION } = {
     });
   },
 };
-
-async function setHeaders({ headers = undefined, type }: iSetHeaders) {
-  // getting token
-  const token =
-    typeof CONFIG.getToken === 'function'
-      ? await CONFIG.getToken()
-      : CONFIG.getToken;
-
-  // if token exits then set it to Authorization else remove it
-  if (token) {
-    // pass false to avoid any prefix
-    const { tokenPrefix = 'Bearer' } = CONFIG;
-    axios.defaults.headers.common['Authorization'] = `${
-      tokenPrefix ? `${tokenPrefix} ` : ''
-    }${token}`;
-  } else {
-    delete axios.defaults.headers.common['Authorization'];
-  }
-
-  // ! Removed: setting other headers
-  // * added headers config in ACTION_HANDLER itself
-  if (typeof headers === 'object' && type) {
-    const actionType = type.toLowerCase() as unknown as string;
-    Object.entries((key: string, value: string) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      axios.defaults.headers[actionType][key] = value;
-    });
-  }
-}
 
 function handleError(error: Error) {
   cache = [];
@@ -177,19 +167,20 @@ export const fetchUrl = ({
   data = {},
   config = {},
 }: iFetchUrl) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     const actionType = type.toUpperCase() as ACTION_TYPES;
-    setHeaders({ headers: config.headers, type });
     url = config.hash ? `${url}?hash=${config.hash}` : url;
     cacheHandler(url);
-
     const handler = ACTION_HANDLERS[actionType];
+    config.headers = await getHeaders(CONFIG, config);
 
     handler(url, data, config)
-      .then((response: AxiosResponse) => resolve(response.data))
+      .then((response: AxiosResponse) => resolve(response?.data))
       .catch((error: Error) => {
         handleError(error);
         return reject(error);
       });
   });
 };
+
+    // cacheHandler(url);
